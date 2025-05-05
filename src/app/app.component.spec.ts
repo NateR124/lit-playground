664727cb83002dc2dc4168522a85c7c424d0bc87
Nate Reynolds <109112677+NateRSE@users.noreply.github.com>
2025-05-05 08:03:16 -0400

    rename files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'my-web-components-playground' title`, () => {
+  it(`should have the 'lit-playground' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-web-components-playground');
+    expect(app.title).toEqual('lit-playground');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-web-components-playground');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, lit-playground');
   });
 });
